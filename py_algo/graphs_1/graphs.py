f51428e0c7d9e8ef81b1283dd3b35bfaf5a55e3d
Sk0uF <skoufas2@hotmail.com>
2021-03-30 18:24:12 +0300

    Added info

--- py_algo/graphs_1/graphs.py
+++ py_algo/graphs_1/graphs.py
@@ -1,3 +1,13 @@
+# 4
+# 5
+# 1 2
+# 2 4
+# 3 1
+# 3 4
+# 4 2
+
+# Connected E >= N-1, without cycles E <= N-1, that's a tree E = N-1.
+
 # Adjacency List
 # If we have a third variable for the weight then we simply
 # add a tuple or a  list of (m, weight) instead of just the edge.
@@ -13,7 +23,6 @@ for i in range(edges):
 
 print(adjacency_list)
 
-
 # Adjacency Martix
 nodes = int(input())
 edges = int(input())
@@ -23,3 +32,34 @@ for i in range(edges):
     adjacency_matrix[n-1][m-1] = 1
 
 print(adjacency_matrix)
+
+
+def bfs(graph, begin):
+    """
+    Breadth First Search
+    Complexity: O(V+Q) => O(V+2*E) => O(V+E)
+
+    Where V are the vertices and Q the sum of the degrees of all nodes.
+    For undirected graphs. It can obviously work with minor adjustments
+    for directed graphs as well. We don't use BFS for weighted graphs
+    and if we do we ignore the weights.
+    """
+    start = 0
+    end = 1
+    queue = [-1] * len(graph)
+    queue[0] = begin
+    visited = [False] * (len(graph)+1)
+    visited[begin] = True
+    while start != end:
+        current = queue[start]
+        queue[start] = -1
+        start += 1
+        print("Node: " + str(current))
+        for i in range(len(graph[current-1])):
+            if not visited[graph[current-1][i]]:
+                queue[end] = graph[current-1][i]
+                visited[graph[current-1][i]] = True
+                end += 1
+
+
+bfs(adjacency_list, 1)
