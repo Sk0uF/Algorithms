72f2cadd1f5cf9385ffeb2e6a931ea059494de7c
Sk0uF <skoufas2@hotmail.com>
2021-04-26 22:12:35 +0300

    Algorithm improvements

--- py_algo/disjoint_data_structures/disjoint_sets.py
+++ py_algo/disjoint_data_structures/disjoint_sets.py
@@ -1,24 +1,41 @@
-def disjoint_sets_array_1(n, m):
-    disjoint_set = [-1] * n
+# 8 7
+# 1 2
+# 3 4
+# 1 3
+# 5 6
+# 7 8
+# 5 8
+# 4 8
+def find_root_and_balance(array, value):
+    # if array[value] != value:
+    #     array[value] = find_root_and_balance(array, array[value])
+    # return array[value]
+
+    while array[value] != value:
+        array[value] = array[array[value]]
+        value = array[value]
+    return array[value]
+
+
+def disjoint_sets_array(n, m):
+    disjoint_set = [i for i in range(n)]
+    sizes = [1] * n
     cycles = 0
     for i in range(m):
         x, y = map(int, input().split())
-        index_x = x-1
-        index_y = y-1
-        while disjoint_set[index_x] >= 0 or disjoint_set[index_y] >= 0:
-            if disjoint_set[index_x] >= 0:
-                index_x = disjoint_set[index_x]
+        root_x = find_root_and_balance(disjoint_set, x-1)
+        root_y = find_root_and_balance(disjoint_set, y-1)
 
-            if disjoint_set[index_y] >= 0:
-                index_y = disjoint_set[index_y]
-
-        if index_x != index_y:
-            if disjoint_set[index_x] <= disjoint_set[index_y]:
-                disjoint_set[index_x] += disjoint_set[index_y]
-                disjoint_set[index_y] = index_x
+        if root_x != root_y:
+            if sizes[root_x] < sizes[root_y]:
+                # disjoint_set[root_y] = root_y
+                # and the same goes for root_x.
+                # Think about it!
+                disjoint_set[root_x] = disjoint_set[root_y]
+                sizes[root_y] += sizes[root_x]
             else:
-                disjoint_set[index_y] += disjoint_set[index_x]
-                disjoint_set[index_x] = index_y
+                disjoint_set[root_y] = disjoint_set[root_x]
+                sizes[root_x] += sizes[root_y]
         else:
             cycles += 1
 
@@ -26,29 +43,5 @@ def disjoint_sets_array_1(n, m):
     print(cycles)
 
 
-def disjoint_sets_array_2(n, m):
-    disjoint_set = []
-    sizes = []
-    for i in range(n):
-        disjoint_set.append(i)
-        sizes.append(1)
-
-    cycles = 0
-    for i in range(m):
-        x, y = map(int, input().split())
-        root_x = disjoint_set[x-1]
-        root_y = disjoint_set[y-1]
-
-        if sizes[root_x] < sizes[root_y]:
-            disjoint_set[root_x] = disjoint_set[root_y]
-            sizes[root_y] += sizes[root_x]
-        else:
-            disjoint_set[root_y] = disjoint_set[root_x]
-            sizes[root_x] += sizes[root_y]
-
-    print(disjoint_set)
-    print(cycles)
-
-
 n, m = map(int, input().split())
-disjoint_sets_array_2(n, m)
+disjoint_sets_array(n, m)
