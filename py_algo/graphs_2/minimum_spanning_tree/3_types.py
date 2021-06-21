838eac22095e237046c8da185b9840a0bd671057
Sk0uF <skoufas2@hotmail.com>
2021-06-22 00:03:18 +0300

    Added explanation.

--- py_algo/graphs_2/minimum_spanning_tree/3_types.py
+++ py_algo/graphs_2/minimum_spanning_tree/3_types.py
@@ -1,3 +1,50 @@
+"""
+Codemonk link: https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/practice-problems/algorithm/3-types/
+
+Let's consider some weird country with N cities and M bidirectional roads of 3 types. It's weird because of some unusual
+rules about using these roads: men can use roads of types 1 and 3 only and women can use roads of types 2 and 3 only.
+Please answer the following very interesting question: what is maximum number of roads it's possible to destroy that the
+country will be still connected for both men and women? Connected country is country where it's possible to travel from
+any city to any other using existing roads.
+
+Input - Output:
+The first line contains 2 space-separated integer: N and M.
+Each of the following M lines contain description of one
+edge: three different space-separated integers: a, b and c.
+a and b are different and from 1 to N each and denote numbers of vertices that are connected by this edge.
+c denotes type of this edge.
+For each test case output one integer - maximum number of roads it's possible to destroy or -1 if the country is not
+connected initially for both men and women.
+
+Sample input:
+5 7
+1 2 3
+2 3 3
+3 4 3
+5 3 2
+5 4 1
+5 2 2
+1 5 1
+
+
+Sample Output:
+2
+"""
+
+"""
+The first thing we need to do is find if there is minimum spanning tree for both men and women. To do that, we create
+2 different graphs, one tha only has edges of types 1 and 3 and the other of types 2 and 3. Now, if there is a MST then
+we would need to do N-1 "merges" using Kruskal's algorithm for each graph. That would take a total of 2N-2 merges. We
+be a little smart and create a graph tha only consists of edges of the 3rd type. If we perform Kruskal's algorithm for 
+that graph (which contains the common paths for both men and women) and find the MST for it, then that would mean that
+if we made K merges for each of the men's and women's graphs. That gives us a total of 2*K merges and we still need 
+2N-2-2K merges if those graphs have a MST. Thus, the answer is TOTAL_EDGES - 2N-2-2K + 2K. That's the total amount of
+roads we can destroy and still have an MST for both women and men.
+
+Final complexity: O(E*INVERSE_ACKERMAN + ElogE)
+"""
+
+
 def balance(disjoint_set, i):
     if disjoint_set[i] != i:
         disjoint_set[i] = balance(disjoint_set, disjoint_set[i])
